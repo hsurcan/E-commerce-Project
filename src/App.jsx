c369489d0182baea46f6f22a81445d2cbfd5899e
Hira Ekin Sürcan <hirasurcan@gmail.com>
2026-04-04 02:12:39 +0300

    changes for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import TopBar from "./components/layout/TopBar";
-import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
+import TopBar from "./components/layout/TopBar.jsx";
+import Navbar from "./components/layout/Navbar.jsx";
+import Footer from "./components/layout/Footer.jsx";
 
 //Pages
-import Home from "./pages/Home";
-import Shop from "./pages/Shop";
-import Product from "./pages/Product";
-import Contact from "./pages/Contact";      
+import Home from "./pages/Home.jsx";
+import Shop from "./pages/Shop.jsx";
+import Contact from "./pages/Contact.jsx";      
 
 
 function App() {
@@ -21,7 +20,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
-          <Route path="/product/:id" element={<Product />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
