37b43be508954ffff71307036e3ce6557641a8bb
Hira Ekin Sürcan <hirasurcan@gmail.com>
2026-04-04 02:03:15 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import Footer from "./components/layout/Footer";
 //Pages
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
-import ProductPage from "./pages/Product";
-import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Contact from "./pages/Contact";      
+import About from "./pages/About";
 
 function App() {
   return (
@@ -16,15 +17,13 @@ function App() {
         <TopBar />
         <Navbar />
       </header>
-
       <main className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
-          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/product/:id" element={<Product />} />
           <Route path="/contact" element={<Contact />} />
-          
-          <Route path="*" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </main>
       <Footer />
